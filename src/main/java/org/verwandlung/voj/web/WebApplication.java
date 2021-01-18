a726ec4ef3b60695f04e8f4011861d8b43d83062
Pipepw <416361536@qq.com>
2021-01-18 19:57:56 +0800

    解决消息队列问题

--- src/main/java/org/verwandlung/voj/web/WebApplication.java
+++ src/main/java/org/verwandlung/voj/web/WebApplication.java
@@ -6,35 +6,17 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.jms.annotation.EnableJms;
 import org.springframework.jms.core.JmsMessagingTemplate;
 import org.springframework.jms.core.JmsTemplate;
 
 import javax.jms.ConnectionFactory;
 
 @SpringBootApplication
+@EnableJms
 public class WebApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(WebApplication.class, args);
-    }
-
-
-    @Value("${spring.activemq.broker-url}")
-    private String ActivemqBrokerUrl;
-    @Bean
-    public ConnectionFactory connectionFactory() {
-        ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory();
-        connectionFactory.setBrokerURL(ActivemqBrokerUrl);
-        return connectionFactory;
-    }
-
-    @Bean
-    public JmsTemplate genJmsTemplate() {
-        return new JmsTemplate(connectionFactory());
-    }
-
-    @Bean
-    public JmsMessagingTemplate jmsMessageTemplate() {
-        return new JmsMessagingTemplate(connectionFactory());
     }
 }
