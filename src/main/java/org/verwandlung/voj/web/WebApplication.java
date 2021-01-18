b97e15bba552656b6bca908317325eb018cd6d12
Pipepw <416361536@qq.com>
2021-01-18 23:10:17 +0800

    真正解决mybatis的问题，properties中的mapper少了个s

--- src/main/java/org/verwandlung/voj/web/WebApplication.java
+++ src/main/java/org/verwandlung/voj/web/WebApplication.java
@@ -1,10 +1,12 @@
 package org.verwandlung.voj.web;
 
 import org.apache.activemq.ActiveMQConnectionFactory;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.jms.annotation.EnableJms;
 import org.springframework.jms.core.JmsMessagingTemplate;
@@ -14,6 +16,7 @@ import javax.jms.ConnectionFactory;
 
 @SpringBootApplication
 @EnableJms
+@MapperScan("org.verwandlung.voj.web.mapper")
 public class WebApplication {
 
     public static void main(String[] args) {
